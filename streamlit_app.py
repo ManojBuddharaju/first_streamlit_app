b0c0063756abc5cb12e0d4b72a3dbfadaa043139
ManojBuddharaju <142869942+ManojBuddharaju@users.noreply.github.com>
2023-08-27 11:10:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,6 +40,9 @@ try:
       back_from_function = get_fruityvice_data(fruit_choice)
       streamlit.dataframe(back_from_function)
 
+# don't run anything past here while we troubleshoot
+streamlit.stop()
+
 except URLError as e:
     streamlit.error()
 
@@ -57,8 +60,6 @@ if streamlit.button('Get Fruit Load List'):
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows) 
 
-# don't run anything past here while we troubleshoot
-streamlit.stop()
 
 # allow the end user to add a fruit to list
 def insert_row_snowflake(new_fruit):
